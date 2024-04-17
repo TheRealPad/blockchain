16265a84f675eda4562782a49bb3c8b6c34475f1
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-04-16 23:19:40 -0700

    feat(initial commit): java project with working blockchain

---
+++ src/main/java/org/example/TransactionInput.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class TransactionInput {
+    public String transactionOutputId;
+    public TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+}
